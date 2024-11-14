1bc54dc4524d5190c39c47d12b8c1eabc6eead70
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2024-11-14 20:51:42 +0100

    Add Win 11 Upgrade

---
+++ Win11Upgrade/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = '\\Zuhause.local\data\GPO\Programme\Win11\setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+
+  softwareName  = 'Win11Upgrade*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/Auto Upgrade /quiet /eula accept /telemetry disable /diagnosticprompt enable /compat ignorewarning'
+  validExitCodes= @(0, 3)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
