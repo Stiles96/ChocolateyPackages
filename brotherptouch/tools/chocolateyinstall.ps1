c9e768bf6be77c96831b4b142fef998b378bed8b
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2024-11-20 09:48:18 +0100

    Add p-touch

---
+++ brotherptouch/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Brother P-Touch Editor 5.4.015.msi'
+$url        = ''
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  file          = $fileLocation
+
+  softwareName  = 'Brother P-touch Editor*'
+
+  checksum      = 'F85A53977B38CF1CD2A9F76D7629BDE3A7051A12D0B81CB573F2CA3C61E44C7B'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
