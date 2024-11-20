542c79dd36bd964e875434d940a09c7be03e2719
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2024-11-20 18:05:33 +0100

    Win7 Games

---
+++ Windows7Games/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Windows7Games.exe'
+$url        = ''
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+
+  softwareName  = 'Windows 7 Games*'
+
+  checksum      = '1A8199A2D52B54019B2353B838C6BEDE5234EBCF8E2615584EDE11D71DE1E1F0'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
