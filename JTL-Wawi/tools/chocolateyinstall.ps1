4e4c2b07ead12863bcdca9c54e663f7f1f3d4a54
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2024-12-17 20:15:32 +0100

    Add JTL, Update DXLog

---
+++ JTL-Wawi/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://downloads.jtl-software.de/setup-jtl-wawi_1.9.6.5_1127-1603_00545d74c8e.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'JTL-Wawi'
+  checksum      = '04E462E1B4ECD96ABD54A1F31F02953F40517D93A81A1EFC1AD7EC69BD08CFE9'
+  checksumType  = 'sha256'
+  silentArgs	 = "/SP- /SILENT /ALLUSERS /NOCANCEL /NORESTART /LOG /NORESTARTAPPLICATIONS /CLOSEAPPLICATIONS"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
