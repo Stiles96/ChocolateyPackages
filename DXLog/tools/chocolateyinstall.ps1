f16ea7a534a3ee0dd73109e6e454c7ec302c570c
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2025-03-04 16:50:22 +0100

    Update Choco

--- DXLog/tools/chocolateyinstall.ps1
+++ DXLog/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://dxlog.net/sw/files/DXLog.net-2.6.12.msi'
-$url64      = 'https://dxlog.net/sw/files/DXLog.net-2.6.12.msi'
+$url        = 'https://dxlog.net/sw/files/DXLog.net-2.6.14.msi'
+$url64      = 'https://dxlog.net/sw/files/DXLog.net-2.6.14.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'DXLog*'
 
-  checksum      = '42C4A09E7BF0F640E46EA75EF81C577779A1328A7D65B7FFA60620D7B9D1B6A8'
+  checksum      = '8E4348197CC5BBB6EE81510D9A401B1DD38712A0B0184269ADA5E113C3E3F62D'
   checksumType  = 'sha256'
-  checksum64    = '42C4A09E7BF0F640E46EA75EF81C577779A1328A7D65B7FFA60620D7B9D1B6A8'
+  checksum64    = '8E4348197CC5BBB6EE81510D9A401B1DD38712A0B0184269ADA5E113C3E3F62D'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
