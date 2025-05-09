995091d0f08d1242119a4e48a91fa3563a0a7cb7
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2025-05-09 17:14:56 +0200

    JTL WaWi Update

--- JTL-Wawi/tools/chocolateyinstall.ps1
+++ JTL-Wawi/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://downloads.jtl-software.de/setup-jtl-wawi_1.9.8.0_0228-0943_82495ec11c0.exe'
+$url           = 'https://downloads.jtl-software.de/setup-jtl-wawi_1.10.10.4_0506-1209_fd098c5005fb.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'JTL-Wawi'
-  checksum      = '59F835D27688F5733525D2E6CD2C0AC40DB49DC157CC13707793C9235794531E'
+  checksum      = 'B04B4EFBD3B679BEB94AFF249546A91CFEE462799AEFCCB9E43C69E551A155D6'
   checksumType  = 'sha256'
   silentArgs	 = "/SP- /SILENT /ALLUSERS /NOCANCEL /NORESTART /LOG /NORESTARTAPPLICATIONS /CLOSEAPPLICATIONS"
   validExitCodes= @(0, 3010, 1641)
