4eb549c2977e786b652de90cde540af9263b212e
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2026-02-28 18:07:19 +0100

    Add MMTTY

---
+++ JTSync/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file64        = "$toolsDir\JTSyncSetup\Setup.exe"
+  file          = "$toolsDir\JTSyncSetup\Setup.exe"
+  softwareName  = 'JTSync*'
+
+  silentArgs    = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+$zipPackageArgs = @{
+  packageName   = "JTSync"
+  unzipLocation = "$toolsDir\JTSyncSetup"
+  url           = 'https://www.dxshell.com/downloads/JTSyncSetup32.zip'
+  url64bit      = 'https://www.dxshell.com/downloads/JTSyncSetup64.zip'
+  checksum      = 'CC366F242D8A79A938C03631D424B14157546BD4239C2E444F0DF914589ECFE5'
+  checksumType  = 'sha256'
+  checksum64    = 'C611604EEB9DF9E66608829757A4B6AB30F1803BF44563C5E36DAD460E2577D9'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @zipPackageArgs
+Install-ChocolateyInstallPackage @packageArgs
---
+++ MMTTY/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = 'https://wiki.he-it.eu/public-uploads/MMTTY170K.exe'
+  url64bit      = 'https://wiki.he-it.eu/public-uploads/MMTTY170K.exe'
+  checksum      = 'CF9F9A365AC05E541CC4E1948A8341948598BF3BFC158BAFBEA4DDB5D3426807'
+  checksum64    = 'CF9F9A365AC05E541CC4E1948A8341948598BF3BFC158BAFBEA4DDB5D3426807'
+  checksumType  = 'sha256'
+  checksumType64= 'sha256'
+  softwareName  = 'MMTTY*'
+
+  silentArgs    = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
