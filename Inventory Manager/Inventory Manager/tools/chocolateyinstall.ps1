7dc8ad8c55b8571f38572853dfd7fc822ff42889
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2026-03-18 11:04:04 +0100

    Add Inventoy Manager

---
+++ Inventory Manager/Inventory Manager/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://inventorymanager.westeurope.cloudapp.azure.com/Inventory%20Manager.msi'
+$url64      = 'https://inventorymanager.westeurope.cloudapp.azure.com/Inventory%20Manager.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Inventory Manager*'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
