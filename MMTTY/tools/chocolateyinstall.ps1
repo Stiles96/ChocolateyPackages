ec7ec375c4a9072bf2916d9b947a753ac6d3a1e9
Patrick Henkelmann <Patrick.Henkelmann@he-it.eu>
2026-04-06 22:06:27 +0200

    Fixes

--- MMTTY/tools/chocolateyinstall.ps1
+++ MMTTY/tools/chocolateyinstall.ps1
@@ -6,11 +6,8 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = 'https://wiki.he-it.eu/public-uploads/MMTTY170K.exe'
-  url64bit      = 'https://wiki.he-it.eu/public-uploads/MMTTY170K.exe'
   checksum      = 'CF9F9A365AC05E541CC4E1948A8341948598BF3BFC158BAFBEA4DDB5D3426807'
-  checksum64    = 'CF9F9A365AC05E541CC4E1948A8341948598BF3BFC158BAFBEA4DDB5D3426807'
   checksumType  = 'sha256'
-  checksumType64= 'sha256'
   softwareName  = 'MMTTY*'
 
   silentArgs    = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL"
